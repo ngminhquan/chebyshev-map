97f0d9aab0f6393fbb13721a9ff1ebf2b70b50ba
Nguyễn Minh Quân <minyingkupo@gmail.com>
2023-04-15 16:57:20 +0700

    fix key agreement

--- hybrid_key_agree.py
+++ hybrid_key_agree.py
@@ -44,7 +44,7 @@ class key_agreement(object):
         return key.decrypt(block)
 
     def verify_user_A(self):        #run at user B
-        digest = hash_function(self._id_a + self._id_b + self.nonce)[:2]
+        digest = hash_function(self._id_b + self._id_a + self.nonce)[:2]
         k_m_new = bytes_to_long(digest)
         if k_m_new != self._k_m:
             return 'INVALID'
@@ -57,29 +57,51 @@ class key_agreement(object):
         self.ssk = long_to_bytes(ssk, 10)
 
         #encrypt session key and send to B
-        cp = self.present_encrypt(self._k_m)
+        cp = self.present_encrypt(long_to_bytes(self._k_m, 8))
         self._cp = bytes_to_long(cp)
+        self._cp_s = bytes_to_long(cp[:2])
 
         #find s = chebyshev(alpha_a - k_m - cp, x) mod p
-        self._s = chebyshev_plus(self._alpha_a - self._k_m - cp, self._x, self._p)
+        self._s = chebyshev_plus(self._alpha_a - self._k_m - self._cp_s, self._x, self._p)
         return self._s, self._cp
 
     def recover_ssk(self):
         #find ssk' from s and cp
-        ssk_new = chebyshev_plus(self._alpha_b + self._cp, self._s, self._p)
+        ssk_new = chebyshev_plus(self._alpha_b + self._cp_s, self._s, self._p)
 
         #decrypt to receive k_m'
-        k_m_test = self.present_decrypt(self._cp)
+        _cp = long_to_bytes(self._cp, 8)
+        k_m_test = self.present_decrypt(_cp)
+        k_m_test = bytes_to_long(k_m_test)
 
         #verify
         if k_m_test == self._k_m:
             print('VALID1')
-            if ssk_new == self.ssk:
+            if ssk_new == bytes_to_long(self.ssk):
                 print("VALID2")
                 return ssk_new
             else:
                 return 'INVALID2'
         else:
             return 'INVALID1'
-        
+
+id_a = b'user_a'
+id_b = b'user_b'
+nonce_i = b'nonce'
+alpha_a = 567040
+alpha_b = 230882
+pu_b = 35903
+x = 842
+p = 46957
+k_m = hash_function(id_b + id_a + nonce_i)[:2]
+k_m = bytes_to_long(k_m)
+
+
+hybrid_key = key_agreement(id_a, id_b, nonce_i, alpha_a, alpha_b, pu_b, x, p, k_m)
+
+a = hybrid_key.verify_user_A()
+b = hybrid_key.gen_ssk()
+c = hybrid_key.recover_ssk()
+
+print(c)
 
